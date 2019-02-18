2032c868840827b285849e066c6632631b3a2984
AbdElrhman Mohamed <superbido185@gmail.com>
2019-02-19 00:39:07 +0200

    docs: adding documentation

--- sol.py
+++ sol.py
@@ -1,9 +1,11 @@
 # superBido ;)
 def power_num(num,powe):
+	"""It takes two numbers and return the value of the first number multiply by itself powe number of time"""
 	result = num ** powe
 	return result
 
 def Power_digit_sum(num,powe):
+	"""it takes 2 number and returns the sum of digit of power_num function"""
 	s_for_sum = str(power_num(num,powe))
 	d_sum = 0
 	for n in s_for_sum:
